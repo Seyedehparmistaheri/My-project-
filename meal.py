62a042b7f859c3b9b9c2dd5464234757353ae30c
Seyedehparmistaheri <parmisstaheri33@gmail.com>
2025-12-06 01:47:48 -0800

    Add files via upload
    A Python program that suggests a meal based on the time of day. The user inputs the current time in HH:MM format, which is then converted to a decimal hour. The program outputs whether it’s breakfast, lunch, or dinner time, or nothing if it falls outside these ranges.

---
+++ meal.py
@@ -0,0 +1,24 @@
+def main():
+    time =input("What time is it?")
+    time = convert(time)
+
+    if 7 <= time <= 8:
+        print("breakfast time")
+
+    elif 12 <= time <= 13:
+        print("Lunch time")
+
+    elif 18 <= time <= 19:
+        print("Dinner time")
+    else:
+        print("")
+
+
+
+def convert(time):
+     hours , minutes = time.split(":")
+
+     return float(hours) +float( minutes)/60
+
+if __name__ == "__main__":
+    main()
